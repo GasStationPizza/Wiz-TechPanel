1532224bc6bffd41f889e9fed71937b1bdd7d31a
GasStationPizza <mrkoester+gh@gmail.com>
2025-05-28 03:43:41 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,7 +14,7 @@ module "ec2_instance" {
   iam_instance_profile = aws_iam_instance_profile.admin_profile.name
   vpc_security_group_ids = [
     aws_security_group.ingress_mongo.id,
-    # aws_security_group.ingress_ssh_all.id,
+    aws_security_group.ingress_ssh_all.id,
     aws_security_group.egress_internet.id
   ]
 
